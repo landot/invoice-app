08059e0f11aae5835968b4771f555f583abb972a
landot <timowland@gmail.com>
2023-06-30 10:44:46 -0700

    move textfield styles

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,36 +1,6 @@
 import { useState } from "react";
-import styled from "styled-components"
 import { TextVariant } from "../styles/text/TextVariant.styles"
-
-export const TextFieldStyles = styled.div`
-    display: flex;
-    flex-direction: column;
-
-    ${TextVariant} {
-        color: ${({ theme }) => theme.colors.grayBlue};
-    }
-`
-
-export const TextInputStyles = styled.input`
-    color: ${({ theme }) => theme.colors.black};
-    font-size: ${({ theme }) => theme.fontSize.medium};
-    font-weight: ${({ theme }) => theme.weight.bold};
-    line-height: ${({ theme }) => theme.lineHeight.extraSmall};
-    letter-spacing: ${({ theme }) => theme.spacing.medium};
-    caret-color: ${({ theme }) => theme.colors.lightPurple};
-    outline: none;
-    height: 48px;
-    width: 240px;
-    padding: 18px 20px 15px 20px;
-    border-radius: 4px;
-    border: ${({ theme }) => `1px solid ${theme.colors.lightGrayBlue}`};
-    background: ${({ theme }) => theme.colors.white};
-    margin-top: 9px;
-
-    &:focus {
-        border: ${({ theme }) => `1px solid ${theme.colors.lightPurple}`};
-    }
-`
+import { TextFieldStyles, TextInputStyles } from "../styles/TextField.styles";
 
 export function TextField(props: {title: string, handleChange?: (value: string) => void}) {
     const [value, setValue] = useState('');
