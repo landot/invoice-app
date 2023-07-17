d6b583d56f7d07e3c5793c3fdf6fa2ab160f253e
landot <timowland@gmail.com>
2023-07-17 15:41:41 -0700

    fix table console errors

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -7,6 +7,7 @@ import { ReactComponent as DeleteIcon } from '../assets/icon-delete.svg';
 import { ItemListStyles, ItemTableStyles, ItemTableRow, ItemNameCellStyles, ItemQuantityCellStyles, ItemPriceCellStyles, ItemTotalCellStyles, ItemDeleteCellStyles } from "../styles/components/ItemList.styles";
 import { StyledButton } from "./Button"
 import { SecondaryButtonStyle } from "../data/types/SecondaryButtonStyle"
+import { convertToCostString } from "../utils/convertToCostString";
 
 export function ItemList(props: {items: Item[], handleItemUpdate?: (updatedItems: Item[]) => void}) {
     const [items, setItems] = useState(props.items);
@@ -14,11 +15,12 @@ export function ItemList(props: {items: Item[], handleItemUpdate?: (updatedItems
     function handleItemUpdate(index: number, field: 'quantity' | 'price' | 'total', value: string) {
         setItems(prev => {
             const newItems = [...prev];
-            const itemToUpdate = newItems[index];
-            itemToUpdate[field] = parseFloat(value);
+            const updatedItem = {...newItems[index]};
+            updatedItem[field] = parseFloat(value);
             if (field === 'quantity' || field === 'price') {
-                itemToUpdate.total = parseFloat((itemToUpdate.price * itemToUpdate.quantity).toString().split(".").map((el,i)=>i?el.split("").slice(0,2).join(""):el).join("."));
+                updatedItem.total = parseFloat((updatedItem.price * updatedItem.quantity).toString().split(".").map((el,i)=>i?el.split("").slice(0,2).join(""):el).join("."));
             }
+            newItems[index] = updatedItem;
             props.handleItemUpdate && props.handleItemUpdate(newItems);
             return newItems;
         })
@@ -60,22 +62,26 @@ export function ItemList(props: {items: Item[], handleItemUpdate?: (updatedItems
         <ItemListStyles>
             <HeadingS>Item List</HeadingS>
             <ItemTableStyles>
-                <ItemTableRow key='header'>
-                    <th><BodyText>Item Name</BodyText></th>
-                    <th><BodyText>Qty.</BodyText></th>
-                    <th><BodyText>Price</BodyText></th>
-                    <th><BodyText>Total</BodyText></th>
-                    <th></th>
-                </ItemTableRow>
-                {items.map((item, index) => (
-                    <ItemTableRow key={index}>
-                        <ItemNameCellStyles><TextField inputType='text' value={item.name} handleChange={(value) => handleItemNameUpdate(index, value)}/></ItemNameCellStyles>
-                        <ItemQuantityCellStyles><TextField inputType='int' value={item.quantity.toString()} handleChange={(value) => handleItemUpdate(index, 'quantity', value)}/></ItemQuantityCellStyles>
-                        <ItemPriceCellStyles><TextField inputType='number' value={item.price.toString()} handleChange={(value) => handleItemUpdate(index, 'price', value)}/></ItemPriceCellStyles>
-                        <ItemTotalCellStyles><HeadingS>{(item.total).toString()}</HeadingS></ItemTotalCellStyles>
-                        <ItemDeleteCellStyles><DeleteIcon onClick={() => handleItemDelete(index)}/></ItemDeleteCellStyles>
+                <thead>
+                    <ItemTableRow>
+                        <th><BodyText>Item Name</BodyText></th>
+                        <th><BodyText>Qty.</BodyText></th>
+                        <th><BodyText>Price</BodyText></th>
+                        <th><BodyText>Total</BodyText></th>
+                        <th></th>
                     </ItemTableRow>
-                ))}
+                </thead>
+                <tbody>
+                    {items.map((item, index) => (
+                        <ItemTableRow key={index}>
+                            <ItemNameCellStyles><TextField inputType='text' value={item.name} handleChange={(value) => handleItemNameUpdate(index, value)}/></ItemNameCellStyles>
+                            <ItemQuantityCellStyles><TextField inputType='int' value={item.quantity.toString()} handleChange={(value) => handleItemUpdate(index, 'quantity', value)}/></ItemQuantityCellStyles>
+                            <ItemPriceCellStyles><TextField inputType='number' value={item.price.toString()} handleChange={(value) => handleItemUpdate(index, 'price', value)}/></ItemPriceCellStyles>
+                            <ItemTotalCellStyles><HeadingS>{convertToCostString(item.total)}</HeadingS></ItemTotalCellStyles>
+                            <ItemDeleteCellStyles><DeleteIcon onClick={() => handleItemDelete(index)}/></ItemDeleteCellStyles>
+                        </ItemTableRow>
+                    ))}
+                </tbody>
             </ItemTableStyles>
             <StyledButton 
                 text="+ Add New Item" 
